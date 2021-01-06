59475b4a57fde5a74561edd0dc24fe58aace64a4
shu353 <shu-3096@hotmail.com>
2021-01-06 11:40:36 -0600

    express challenges

--- server.js
+++ server.js
@@ -6,6 +6,8 @@
 "use strict";
 
 var fs = require("fs");
+var bGround = require("fcc-express-bground");
+var myApp = require("./myApp");
 var express = require("express");
 var app = express();
 
@@ -58,6 +60,7 @@ app.use(function (err, req, res, next) {
   }
 });
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Node.js listening ...");
+var port = process.env.PORT || 3000;
+bGround.setupBackgroundApp(app, myApp, __dirname).listen(port, function () {
+  bGround.log("Node is listening on port " + port + "...");
 });
