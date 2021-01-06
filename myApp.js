864e98e3812e84fe45619af7c693fcf2d10f7452
shu353 <shu-3096@hotmail.com>
2021-01-06 12:03:31 -0600

    console bground

--- myApp.js
+++ myApp.js
@@ -1,5 +1,8 @@
 var express = require("express");
 var app = express();
+var bGround = require("fcc-express-bground");
 
 console.log("Hello World");
+console.log("Hello World");
+bGround.log("Hello World");
 module.exports = app;
